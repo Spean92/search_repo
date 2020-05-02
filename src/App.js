60ca6d1b1b92d9110182288d2838ed8e7db2d3ee
Yegor <egor.muhladi@gmal.com>
2020-05-02 11:05:50 +0300

    fetch and display data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {API_URL} from "./utils";
 
-function App() {
-  return (
-    <div className="App">
+export const App = () => {
+    const [totalResult, setTotalResult] = useState(0);
+    const [repos, setRepos] = useState([]);
 
-    </div>
-  );
-}
+    const fetchData = async (query, page = 1, count = 30) => {
+        try {
+            const response = await fetch(`${API_URL}/search/repositories?q=${query}&sort=stars&page=${page}&per_page=${count}`);
+            const jsonData = await response.json();
+            console.log(jsonData);
+            setTotalResult(jsonData.total_count);
+            setRepos(jsonData.items);
+        } catch (e) {
+            throw new Error(`Error ${e}`)
+        }
 
-export default App;
+    };
+    useEffect(() => {
+        fetchData(`test`);
+    }, []);
+
+    return (
+        <div>
+            <p>Total found - {totalResult}</p>
+            <ol>{repos.map(({id, full_name, html_url}) => (
+                <li key={id}><a href={html_url}>{full_name}</a></li>
+            ))}</ol>
+        </div>
+    );
+};
