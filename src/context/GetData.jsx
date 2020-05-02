006ab182bf20ea08e20754c87fcbcfa5f9c91690
Yegor <egor.muhladi@gmal.com>
2020-05-02 14:29:53 +0300

    fix search by Name

--- src/context/GetData.jsx
+++ src/context/GetData.jsx
@@ -6,7 +6,7 @@ export const GetData = ({children}) => {
     const [data, setData] = useState({});
     const fetchData = async (query, page = 1, count = 30) => {
         //TODO loader
-        const response = await fetch(`${API_URL}/search/repositories?q=${query}&sort=stars&page=${page}&per_page=${count}`);
+        const response = await fetch(`${API_URL}/search/repositories?q=${query}+in:name&sort=stars&page=${page}&per_page=${count}`);
         const jsonData = await response.json();
         setData(jsonData);
     };
