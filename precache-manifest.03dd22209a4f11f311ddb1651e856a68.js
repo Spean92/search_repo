3b674f85dff826f5ef09245627c9b1e7d2ee9c32
Yegor <egor.muhladi@gmal.com>
2020-05-04 01:07:19 +0300

    Updates

---
+++ precache-manifest.03dd22209a4f11f311ddb1651e856a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d69261380ebd8a1dab355a0df06c8d",
+    "url": "/search_repo/index.html"
+  },
+  {
+    "revision": "e26e7e02c0c5a1a2200a",
+    "url": "/search_repo/static/css/main.6f95fca8.chunk.css"
+  },
+  {
+    "revision": "94dd7651914bfcb23ad1",
+    "url": "/search_repo/static/js/2.6b10e40c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search_repo/static/js/2.6b10e40c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e26e7e02c0c5a1a2200a",
+    "url": "/search_repo/static/js/main.0c002ca8.chunk.js"
+  },
+  {
+    "revision": "e602ccc0e965dc3a450f",
+    "url": "/search_repo/static/js/runtime-main.282b9c54.js"
+  }
+]);
