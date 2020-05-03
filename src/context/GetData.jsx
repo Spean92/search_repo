b85043a0d876f14133e68c9776ad82afee838e66
Yegor <egor.muhladi@gmal.com>
2020-05-03 13:59:42 +0300

    add auth token

--- src/context/GetData.jsx
+++ src/context/GetData.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {API_URL} from "../utils";
+import {API_URL, AUTH_HEADER} from "../utils";
 import {DataContext} from "./DataContext";
 
 export const GetData = ({children}) => {
@@ -8,7 +8,7 @@ export const GetData = ({children}) => {
     const [currentQuery, setCurrentQuery] = useState(``);
     const fetchData = async (query, page = 1, count = 30) => {
         //TODO loader
-        const response = await fetch(`${API_URL}/search/repositories?q=${query}+in:name&sort=stars&page=${page}&per_page=${count}`);
+        const response = await fetch(`${API_URL}/search/repositories?q=${query}+in:name&sort=stars&page=${page}&per_page=${count}`, AUTH_HEADER);
         const jsonData = await response.json();
         setData(jsonData);
         setCurrentQuery(query);
