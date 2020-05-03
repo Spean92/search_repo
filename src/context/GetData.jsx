194ba76625b4df6eec8f7373017a91e7ac6e9565
Yegor <egor.muhladi@gmal.com>
2020-05-03 17:57:08 +0300

    setUp cache requests

--- src/context/GetData.jsx
+++ src/context/GetData.jsx
@@ -7,9 +7,18 @@ export const GetData = ({children}) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [currentQuery, setCurrentQuery] = useState(``);
     const fetchData = async (query, page = 1, count = 30) => {
+        const url = `${API_URL}/search/repositories?q=${query}+in:name&sort=stars&page=${page}&per_page=${count}`;
+        const cache = await caches.open('github-api-requests');
+        let jsonData = {};
         //TODO loader
-        const response = await fetch(`${API_URL}/search/repositories?q=${query}+in:name&sort=stars&page=${page}&per_page=${count}`, AUTH_HEADER);
-        const jsonData = await response.json();
+        const getCache = await cache.match(url);
+        if (getCache) {
+            jsonData = await getCache.json();
+        } else {
+            const responseFromServer = await fetch(url);
+            jsonData = await responseFromServer.clone().json();
+            cache.put(url, responseFromServer);
+        }
         setData(jsonData);
         setCurrentQuery(query);
         setCurrentPage(page);
